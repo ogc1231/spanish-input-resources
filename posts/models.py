9929f5d19100c8eabd602ce96fdd955b4cb78f9a
ogc1231 <ogc@live.ie>
2023-08-23 14:56:06 +0000

    Add post serilaizer and model

--- posts/models.py
+++ posts/models.py
@@ -1,28 +1,25 @@
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
-class Profile(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+class Post(models.Model):
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_fw7hkl', blank=True
+        upload_to='images/', default='../default_post_cvimec', blank=True
     )
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.owner}'s profile"
+        return f'{self.id} {self.title}'
 
 
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
-
-
-post_save.connect(create_profile, sender=User)
