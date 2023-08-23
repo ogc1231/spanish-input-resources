df6e99dfe9e1c26cac20d95171647a15d8d0f89c
ogc1231 <ogc@live.ie>
2023-08-23 17:35:14 +0000

    Link heroku and database

--- sir_api/settings.py
+++ sir_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -53,12 +54,12 @@ REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'sir_api.serializers.Current
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hyys-+kq#d#)((b_7qp1$*u)atx1h9mvyn5=-w8vi^5zo@4s@j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-ogc1231-spanishinputres-i31xwtv6vln.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['spanish-input-resources.herokuapp.com', 'localhost', '8000-ogc1231-spanishinputres-i31xwtv6vln.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -81,7 +82,7 @@ INSTALLED_APPS = [
     'allauth.account', 
     'allauth.socialaccount', 
     'dj_rest_auth.registration',
-
+    'corsheaders',
     'profiles',
     'posts',
     'comments',
@@ -90,6 +91,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,6 +100,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
+JWT_AUTH_SAMESITE = 'None'
 
 ROOT_URLCONF = 'sir_api.urls'
 
@@ -124,10 +138,12 @@ WSGI_APPLICATION = 'sir_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': ({
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    } if 'DEV' in os.environ else dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    ))
 }
 
 
