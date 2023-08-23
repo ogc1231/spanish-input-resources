533d1d4b2be95549a2101c9a885c02a626f84a75
ogc1231 <ogc@live.ie>
2023-08-23 18:04:46 +0000

    Fix restframework.renderers.JSONRenderer typo

--- sir_api/settings.py
+++ sir_api/settings.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
 }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'restframework.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
     ]
 
 REST_USE_JWT = True
