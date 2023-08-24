e2ac3dab4079cd21e2503a01e52c5083324be3c1
ogc1231 <ogc@live.ie>
2023-08-24 08:49:02 +0000

    Update database code

--- sir_api/settings.py
+++ sir_api/settings.py
@@ -137,18 +137,17 @@ WSGI_APPLICATION = 'sir_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.getenv("DEV"):
+if 'DEV' in os.environ:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }  
+    }
 else:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-        }
-
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
