d92151e7847052c48b1fe7e4c81365f9e6f0e679
ogc1231 <ogc@live.ie>
2023-08-24 08:43:31 +0000

    Update linking database code

--- sir_api/settings.py
+++ sir_api/settings.py
@@ -137,14 +137,17 @@ WSGI_APPLICATION = 'sir_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': ({
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    ))
-}
+if os.getenv("DEV"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }  
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        }
 
 
 # Password validation
