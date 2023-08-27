a5e44d4d9caa6a96b6071ee45bc7fcd9b738f1b1
ogc1231 <ogc@live.ie>
2023-08-27 14:32:42 +0000

    Setup and style signup form

--- sir_frontend/src/pages/auth/SignUpForm.js
+++ sir_frontend/src/pages/auth/SignUpForm.js
@@ -14,7 +14,26 @@ const SignUpForm = () => {
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-            {/* add your form here */}
+        <Form>
+            <Form.Group className="mb-3" controlId="username">
+                <Form.Label className='d-none'>Username</Form.Label>
+                <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Label className='d-none'>Password</Form.Label>
+                <Form.Control className={styles.Input} type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="passwordcon">
+                <Form.Label className='d-none'>Confirm Password</Form.Label>
+                <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="passwordcon" />
+            </Form.Group>
+
+            <Button className={`${btnStyles.Button} ${btnStyles.Wide}`} type="submit">
+                Sign Up
+            </Button>
+        </Form>
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
