56304a0ff338fa86fa5b4e9c08a2d1115d2b09b0
ogc1231 <ogc@live.ie>
2023-08-29 09:38:39 +0000

    Create axiosReq & axiosRes interceptors

--- sir_frontend/src/api/axiosDefaults.js
+++ sir_frontend/src/api/axiosDefaults.js
@@ -11,3 +11,6 @@ import axios from 'axios'
 axios.defaults.baseURL = "/api"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create()
+export const axiosRes = axios.create()
