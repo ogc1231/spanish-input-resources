669b6b10b32cfa44eb842b4740c35c32e623e500
ogc1231 <ogc@live.ie>
2023-08-29 08:48:57 +0000

    Allow user to login and see name when logged in

--- sir_frontend/src/App.js
+++ sir_frontend/src/App.js
@@ -1,12 +1,15 @@
-import styles from './App.module.css';
+import styles from './App.module.css'
 import NavBar from './components/NavBar'
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'
 import {Route, Switch} from 'react-router-dom'
 import './api/axiosDefaults'
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import SignUpForm from './pages/auth/SignUpForm'
+import SignInForm from './pages/auth/SignInForm'
+import { useEffect, useState, createContext } from 'react'
+import axios from 'axios'
+
+export const CurrentUserContext = createContext()
+export const SetCurrentUserContext = createContext()
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null)
@@ -24,19 +27,23 @@ function App() {
   }, [])
 
   return (
-    <div className={styles.App}>
-      <NavBar/>
-      <Container>
-        <Switch>
-          <Route exact path='/' render={() => <h1>Home</h1>}/>
-          <Route exact path='/resources' render={() => <h1>Resources</h1>}/>
-          <Route exact path='/signin' render={() => <SignInForm/>}/>
-          <Route exact path='/signup' render={() => <SignUpForm/>}/>
-          <Route render={() => <p>Page Not Found!</p>}/>
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar/>
+          <Container>
+            <Switch>
+              <Route exact path='/' render={() => <h1>Home</h1>}/>
+              <Route exact path='/resources' render={() => <h1>Resources</h1>}/>
+              <Route exact path='/signin' render={() => <SignInForm/>}/>
+              <Route exact path='/signup' render={() => <SignUpForm/>}/>
+              <Route render={() => <p>Page Not Found!</p>}/>
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
-export default App;
+export default App
--- sir_frontend/src/components/NavBar.js
+++ sir_frontend/src/components/NavBar.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import favicon from '../assets/favicon.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 export default function NavBar() {
-    return (
-        <Navbar className={styles.NavBar} collapseOnSelect expand="lg">
+  const currentUser = useContext(CurrentUserContext)
+  const loggedInIcons =  <>{currentUser?.username}</>
+  const loggedOutIcons = (
+    <>
+      <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signin'><i className="fa-solid fa-right-to-bracket"></i>Sign In</NavLink>
+      <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signup'><i className="fa-solid fa-user-plus"></i>Sign Up</NavLink>
+    </>
+  )
+
+  return (
+    <Navbar className={styles.NavBar} collapseOnSelect expand="lg">
       <Container>
         <NavLink className={styles.NavLink} to='/'>
             <Navbar.Brand><img src={favicon} alt={favicon} height='45'></img>Spanish Input Resources</Navbar.Brand>
@@ -20,11 +30,10 @@ export default function NavBar() {
             <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/resources'><i className="fa-solid fa-podcast"></i>Resources</NavLink>
           </Nav>
           <Nav className="mr-auto text-left">
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signin'><i className="fa-solid fa-right-to-bracket"></i>Sign In</NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signup'><i className="fa-solid fa-user-plus"></i>Sign Up</NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    )
+  )
 }
